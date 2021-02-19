ba2352238070e18b89e478a34508de7a09ed6b66
Gomana <gomanaes@gmail.com>
2021-02-19 11:00:25 +0200

    added usregiter and login user and returning a token

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,10 @@
 const ErrorResponse= require('../utils/errorResponse');
 const asyncHandler= require('../middleware/async');
 const User= require('../models/User');
+const e = require('express');
 
 //@description Register user
-//@route Get /api/v1/register
+//@route Post /api/v1/register
 //@access Public
 exports.register = asyncHandler(async(req,res,next)=>{
     const {name, email, password, role}= req.body;
@@ -15,5 +16,39 @@ exports.register = asyncHandler(async(req,res,next)=>{
         password,
         role
     });
-    res.status(200).json({success:true});
+
+    //create token
+    const token = user.getSignedJwtToken();
+    res.status(200).json({success:true, token});
+});
+
+//@description Login user
+//@route Post /api/v1/login
+//@access Public
+exports.login = asyncHandler(async(req,res,next)=>{
+    const {email, password}= req.body;
+//validate email and password
+
+if(!email|| !password){
+return next (new ErrorResponse('please provide an email and password', 400));
+
+}
+//Check for user
+const user= await User.findOne({email}).select('+password');
+if(!user){
+    return next (new ErrorResponse('Invalid Credentials', 401)); 
+}
+   
+// check if password matches
+const isMatch= await user.matchPassword(password);
+
+if (!isMatch){
+    return next (new ErrorResponse('Invalid Credentials', 401)); 
+
+}
+
+
+//create token
+    const token = user.getSignedJwtToken();
+    res.status(200).json({success:true, token});
 });
